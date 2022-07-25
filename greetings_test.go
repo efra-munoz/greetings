9668d6588726f2cc96cd993b3124da2f5355f81e
Efraín Muñoz Coreano <efra@eframunoz.com>
2022-07-25 12:39:03 -0400

    Change 'Hello' function to return message and error.

--- greetings_test.go
+++ greetings_test.go
@@ -1,14 +1,28 @@
 package greetings
 
-import "testing"
+import (
+	"testing"
+)
+
+func expectEqual(t testing.TB, result, expected interface{}) {
+	t.Helper()
+	if result != expected {
+		t.Errorf("expected %q but got %q", expected, result)
+	}
+}
 
 func TestHello(t *testing.T) {
 	t.Run("should create greeting to <name>", func(t *testing.T) {
-		result := Hello("Gara")
+		result, _ := Hello("Gara")
 		expected := "Hi, Gara. Welcome!"
 
-		if result != expected {
-			t.Errorf("expected %q but got %q", expected, result)
-		}
+		expectEqual(t, result, expected)
+	})
+
+	t.Run("should return error if <name> is empty", func(t *testing.T) {
+		_, error := Hello("")
+		expected_error := ErrorEmptyName.Error()
+
+		expectEqual(t, error.Error(), expected_error)
 	})
 }
