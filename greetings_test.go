543280914f2ce7b3ea1a6fe5e89cc133226c7dae
Efraín Muñoz Coreano <efra@eframunoz.com>
2022-07-25 11:38:20 -0400

    Refactor Hello test.

--- greetings_test.go
+++ greetings_test.go
@@ -3,10 +3,12 @@ package greetings
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello("Gara")
-	expected := "Hi, Gara. Welcome!"
+	t.Run("should create greeting to <name>", func(t *testing.T) {
+		result := Hello("Gara")
+		expected := "Hi, Gara. Welcome!"
 
-	if result != expected {
-		t.Errorf("expected %q but got %q", expected, result)
-	}
+		if result != expected {
+			t.Errorf("expected %q but got %q", expected, result)
+		}
+	})
 }
